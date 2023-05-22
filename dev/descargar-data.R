623077fcaa80d3e082ece99da461550397d2b55f
Joshua Kunst <jbkunst@gmail.com>
2023-05-21 20:21:33 -0400

    Create descargar-data.R

---
+++ dev/descargar-data.R
@@ -0,0 +1,38 @@
+# devtools::load_all()
+library(tidyverse)
+library(cmfr)
+
+try(dir.create("data-raw/balance"))
+
+dlinks <- cmf_besb_links()
+
+dlinks |>
+  # head() |>
+  select(fecha, link) |>
+  pwalk(function(fecha, link){
+
+    # fecha <- lubridate::ymd("2023-03-01")
+    # link  <- "https://www.cmfchile.cl/portal/estadisticas/617/articles-69329_recurso_1.zip"
+
+    cli::cli_progress_step("{format(fecha, \"%Y%m\")}")
+
+    fout <- str_glue("data-raw/balance/{format(fecha, \"%Y%m\")}.rds")
+
+    if(fs::file_exists(fout)) return(TRUE)
+
+    daux <- cmf_besb_data(link)
+
+    saveRDS(daux, fout)
+
+  })
+
+data <- dir("data-raw/balance", full.names = TRUE) |>
+  map(readRDS)
+
+data |> map(count) |>  bind_rows()
+
+data |> map(count, periodo) |>  bind_rows()
+
+data |>
+  bind_rows() |>
+  filter(modelo == "b1", cod_ifi == "001", X1 == 100000000)
